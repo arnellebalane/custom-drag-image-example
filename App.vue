700b94106e4bcf85838d25f29d54d7342385ac9a
Arnelle Balane <arnellebalane@gmail.com>
2018-06-05 07:53:55 +0800

    Define PhotoGrid component

--- App.vue
+++ App.vue
@@ -1,11 +1,58 @@
 <template>
     <div class="app" id="app">
-        <h1>Custom Drag Image Example</h1>
+        <div class="wrapper">
+            <header>
+                <h1>Custom Drag Image Example</h1>
+                <a href="https://github.com/arnellebalane/custom-drag-image-example" target="_blank" rel="noopener noreferrer">
+                    github.com/arnellebalane/custom-drag-image-example
+                </a>
+            </header>
+
+            <main>
+                <PhotoGrid />
+            </main>
+        </div>
     </div>
 </template>
 
 <script>
+    import PhotoGrid from './components/PhotoGrid.vue';
+
     export default {
-        name: 'App'
+        name: 'App',
+
+        components: {
+            PhotoGrid
+        }
     };
 </script>
+
+<style scoped>
+    .wrapper {
+        width: 100%;
+        max-width: 800px;
+        margin: 0 auto;
+    }
+
+    header {
+        margin-bottom: 50px;
+        text-align: center;
+    }
+
+    header h1 {
+        font-size: 24px;
+    }
+
+    header a {
+        font-size: 14px;
+        color: #888;
+    }
+
+    main {
+        display: flex;
+    }
+
+    .photo-grid {
+        flex: 1 0 0;
+    }
+</style>
---
+++ components/PhotoGrid.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="photo-grid">
+        <div v-for="photo in photos" :key="photo" class="photo">
+            <img :src="photo" alt="Photo grid item" />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PhotoGrid',
+
+        data() {
+            return {
+                photos: [
+                    require('../images/01.jpg'),
+                    require('../images/02.jpg'),
+                    require('../images/03.jpg'),
+                    require('../images/04.jpg'),
+                    require('../images/05.jpg'),
+                    require('../images/06.jpg'),
+                    require('../images/07.jpg'),
+                    require('../images/08.jpg'),
+                    require('../images/09.jpg'),
+                    require('../images/10.jpg'),
+                ]
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    .photo-grid {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        padding: 5px;
+    }
+
+    .photo {
+        width: 150px;
+        margin: 5px;
+        cursor: pointer;
+        overflow: hidden;
+    }
+
+    img {
+        display: block;
+        width: 100%;
+        transition: transform 150ms ease-out;
+    }
+
+    .photo:hover img {
+        transform: scale(1.1);
+    }
+</style>
