faa4485a416e98c9471bc8987fb1d3c8fe882f2e
Arnelle Balane <arnellebalane@gmail.com>
2018-06-05 07:31:21 +0800

    Initial project setup

---
+++ App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="app" id="app">
+        <h1>Custom Drag Image Example</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    };
+</script>
