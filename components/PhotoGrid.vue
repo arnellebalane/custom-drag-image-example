38a3a220ca454e004687aee575a93f2e7a938839
Arnelle Balane <arnellebalane@gmail.com>
2018-06-05 09:30:04 +0800

    Generate drag image based on dragged images

--- components/PhotoGrid.vue
+++ components/PhotoGrid.vue
@@ -6,6 +6,7 @@
             :class="{selected: isSelected(photo)}"
             @click="toggle(photo)"
             @dragstart="onDragStart($event, photo)"
+            @dragend="onDragEnd"
             class="photo"
             draggable="true"
         >
@@ -32,8 +33,17 @@
                     require('../images/09.jpg'),
                     require('../images/10.jpg'),
                 ],
-                selectedPhotos: []
+                selectedPhotos: [],
+                canvas: null
             };
+        },
+
+        watch: {
+            canvas(value, oldValue) {
+                if (!value) {
+                    oldValue.remove();
+                }
+            }
         },
 
         methods: {
@@ -70,6 +80,73 @@
 
                 e.dataTransfer.setData('text/uri-list', links);
                 e.dataTransfer.setData('text/plain', links);
+
+                e.dataTransfer.setDragImage(this.generateDragImage(links), 20, 20);
+            },
+
+            onDragEnd(e) {
+                // If dropped on a valid drop zone, dropEffect will not be none
+                if (e.dataTransfer.dropEffect !== 'none') {
+                    this.selectedPhotos = [];
+                }
+
+                this.canvas = null;
+            },
+
+            generateDragImage(links) {
+                const images = links.split(/\n*#\n*/g).map(link => {
+                    const image = new Image();
+                    image.src = link;
+                    return image;
+                });
+
+                const canvas = document.createElement('canvas');
+                canvas.width = 120;
+                const context = canvas.getContext('2d');
+
+                if (images.length === 1) {
+                    canvas.height = 80;
+
+                    context.drawImage(images[0], 0, 0);
+                } else {
+                    const isEven = images.length % 2 === 0;
+                    canvas.height = isEven
+                        ? Math.ceil(images.length / 2) * 40
+                        : (Math.ceil(images.length / 2) + 1) * 40;
+
+                    images.forEach((image, i) => {
+                        let x, y;
+                        let width = 60;
+                        let height = 40;
+
+                        if (isEven) {
+                            x = i % 2;
+                            y = Math.floor(i / 2);
+                        } else {
+                            if (i === 0) {
+                                x = 0;
+                                y = 0;
+                                width = 120;
+                                height = 80;
+                            } else {
+                                x = (i - 1) % 2;
+                                y = Math.floor((i - 1) / 2) + 2;
+                            }
+                        }
+
+                        context.drawImage(image, 60 * x, 40 * y, width, height);
+                    });
+                }
+
+
+                this.canvas = canvas;
+                this.$el.appendChild(canvas);
+                canvas.style.backgroundColor = 'transparent';
+                canvas.style.position = 'absolute';
+                canvas.style.top = '-100vh';
+                canvas.style.left = '-100vw';
+
+                return canvas;
             }
         }
     };
@@ -107,5 +184,6 @@
         display: block;
         width: 100%;
         border-radius: 3px;
+        pointer-events: none;
     }
 </style>
